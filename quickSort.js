4135856e6e295c70140411ca226fda4c524d3ce8
ShiTuoCheng <shituocheng@gmail.com>
2018-08-23 16:40:50 +0800

    add

--- quickSort.js
+++ quickSort.js
@@ -47,82 +47,37 @@ const swap = (array, i, j) => {
 //   return arr;
 // }
 
-const arr = [2,3,1,4,5];
-
 const partition = (arr, low, high) => {
+  let pivot = arr[low]
 
-  let pivot = arr[low];
+  while (low < high) {
 
-  while(low < high){
-
-    while (low < high && arr[low] <= pivot  ) {
-      ++low;
+    while (low < high && arr[high] > pivot) {
+      --high
     }
 
-    // 交换
-    arr[high] = arr[low];
+    arr[low] = arr[high]
 
-    while(low < high && pivot < arr[high]){
-      --high;
+    while (low < high && arr[low] <= pivot) {
+      ++low
     }
 
-    arr[low] = arr[high];
+    arr[high] = arr[low]
   }
 
-  arr[low] = pivot;
-  return low;
-};
+  arr[low] = pivot
+  return low
+}
 
 const quickSort = (arr, low, high) => {
+  if (low < high) {
 
-  if(low < high){
-
-    let pivot = partition(arr, low, high);
-    quickSort(arr, pivot + 1, high);
-    quickSort(arr, low, pivot - 1);
+    let pivot = partition(arr, low, high)
+    quickSort(arr, low, pivot - 1)
+    quickSort(arr, pivot + 1, high)
   }
-
-  return arr;
-};
-
-// const partition = (arr, low, high) => {
-
-//   let pivot = arr[low];
-
-//   while(low < high){
-
-//     while(low < high && arr[high] > pivot){
-
-//       --high;
-//     }
-
-//     arr[low] = arr[high];
-
-//     while(low < high && arr[low] <= pivot){
-
-//       ++low;
-//     }
-
-//     arr[high] = arr[low];
-//   }
-
-//   arr[low] = pivot;
-//   return low;
-// }
-
-// const quickSort = (arr, low, high) => {
-
-//   if(low < high){
-
-//     let pivot = partition(arr, low, high);
-
-//     quickSort(arr, low, pivot - 1);
-//     quickSort(arr, pivot + 1, high);
-//   }
-
-//   return arr;
-// }
+}
 
 
 
-console.log(quickSort(arr, 0, 4));
+// console.log(quickSort(arr, 0, 4));

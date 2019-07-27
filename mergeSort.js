c14349284843243b4d78282aabe95f5092cb6f49
ShiTuoCheng <shituocheng@gmail.com>
2019-07-27 20:42:35 +0800

    merge sort

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+// 归并排序
+
+const mergeSort = arr => {
+  const len = arr.length;
+  if (len < 2) return arr;
+
+  const middle = Math.floor(len/2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle, len);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+const merge = (left, right) => {
+  const result = [];
+  while(left.length && right.length) {
+    if (left[0] <= right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while(left.length) {
+    result.push(left.shift());
+  }
+
+  while(right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
+}
+
+console.log(mergeSort([3,5,2,1,2,4]));
